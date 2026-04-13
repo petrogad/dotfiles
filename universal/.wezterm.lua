375fb60705423f0480404ceb928607b2ac8a87df
Pete <petrogad@gmail.com>
2026-04-13 13:46:34 -0500

    fixed font

--- universal/.wezterm.lua
+++ universal/.wezterm.lua
@@ -5,7 +5,7 @@ local config = wezterm.config_builder()
 config.color_scheme = "Catppuccin Mocha"
 
 config.font = wezterm.font("FiraCode Nerd Font")
-config.font_size = 16
+config.font_size = 13
 
 config.default_prog = { "/bin/zsh", "-l" }
 
